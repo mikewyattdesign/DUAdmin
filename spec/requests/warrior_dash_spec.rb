6dc08a9ff4d20742db624a957db2105f93a21ec5
Michael Wyatt <michaelwyatt@group360.com>
2014-07-01 10:57:53 -0500

    Add multiple submission request spec

--- spec/requests/warrior_dash_spec.rb
+++ spec/requests/warrior_dash_spec.rb
@@ -41,4 +41,45 @@ describe 'Warrior Dash API' do
         # expect entrant to come back
         expect(result[:entrants].length).to eq(1)
     end
+
+    it 'Accepts multiple submissions' do 
+        NUM_ENTRIES = 500
+        data = {}
+
+        (1..NUM_ENTRIES).to_a.each do | n |
+        guid = SecureRandom.uuid
+        entry = build(:entry, entrant_guid: guid)
+        entrant = build(:entrant, guid: guid)
+        data["#{guid}"] = {
+            entry: {
+                s3_uri: entry.s3_uri,
+                date_created: entry.date_created
+                },
+            entrant: {
+                name: entrant.name,
+                phone: entrant.phone,
+                email: entrant.email,
+                birthdate: entrant.birthdate,
+                street_address: entrant.street_address,
+                zipcode: entrant.zipcode
+            }
+        }
+        end
+        post '/api/warriordash',
+        data.to_json,
+        { 'Accept' => Mime::JSON, 'Content-Type' => Mime::JSON.to_s}
+
+        # expect a 201 status code
+        expect(response.status).to eq(201)
+
+        # get response
+        result = json(response.body)
+
+        # expect entry to come back
+        expect(result[:entries].length).to eq(NUM_ENTRIES)
+
+        # expect entrant to come back
+        expect(result[:entrants].length).to eq(NUM_ENTRIES)
+
+    end
 end
