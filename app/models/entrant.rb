a8753983469255d400d5d33e6cfe920b3293a93c
Michael Wyatt <michaelwyatt@group360.com>
2014-07-28 15:12:10 -0500

    Only require name and email.

--- app/models/entrant.rb
+++ app/models/entrant.rb
@@ -1,8 +1,7 @@
 class Entrant < ActiveRecord::Base
     AGE_OF_RESPONSIBILITY = 21
 
-    validates_presence_of :name, :birthdate, :street_address, :zipcode
-    validates :phone, presence: true, phone: true
+    validates_presence_of :name
     validates :email, presence: true
 
     has_many :entries
--- spec/models/entrant_spec.rb
+++ spec/models/entrant_spec.rb
@@ -9,17 +9,6 @@ describe Entrant do
     expect(build(:entrant, name: nil)).to have(1).errors_on(:name)
   end
 
-  # Phone Number
-  it 'is valid with a phone number' do
-    expect(build(:entrant)).to be_valid
-  end
-  it 'is invalid without a phone number' do
-    expect(build(:entrant, phone: nil)).to have_at_least(1).errors_on(:phone)
-  end
-  it 'is invalid with an invalid phone number' do
-    expect(build(:entrant, phone: "1234")).to have(1).errors_on(:phone)
-  end
-
   # Email
   it 'is valid with a valid email' do
     expect(build(:entrant)).to be_valid
@@ -29,34 +18,11 @@ describe Entrant do
   end
 
   # Birthdate
-  it 'is valid with a valid date of birth' do
-    expect(build(:entrant)).to be_valid
-  end
-  it 'is invalid without a date of birth' do
-    expect(build(:entrant, birthdate: nil)).to have(1).errors_on(:birthdate)
-  end
   it 'knows when it is underage' do 
     expect(build(:entrant, birthdate: Date.today).underage?).to be_truthy
   end
-
   it 'knows when it is not underage' do
     expect(build(:entrant).underage?).to be_falsey
-  end
-
-  # Street Address
-  it 'is valid with a street address' do
-    expect(build(:entrant)).to be_valid
-  end
-  it 'is invalid without a street address' do
-    expect(build(:entrant, street_address: nil)).to have(1).errors_on(:street_address)
-  end
-
-  # Zipcode
-  it 'is valid with a zipcode' do
-    expect(build(:entrant)).to be_valid
-  end
-  it 'is invalid without a zipcode' do 
-    expect(build(:entrant, zipcode: nil)).to have(1).errors_on(:zipcode)
   end
 
   it 'responds to entries' do
